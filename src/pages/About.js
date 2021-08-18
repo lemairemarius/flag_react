c905ce1574916ac7b1ddc8177fd9ea752e439839
Miguigno <lemairemarius>
2021-08-18 15:15:58 +0200

    done project

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <Logo />
+            <h1>À propos</h1>
+            <br />
+            <p>I got a surprise for you, Morty. I didn't. I did. Oh, man Morty. How DID she get there? Well, I-I-I think we've seen enough. I'll just figure out how to unplug this. Put them way up inside there, as far as they can fit. I could--It could be Morty.</p>
+            <br />
+            <p>Absolutely. Uh, I was also late because of my drinking and metioned it to zero applause. A hair, Morty, I need one of your hairs! This isn't Game Of Thrones. I think planet Earth has just been transported. Mission accomplished, boys. Pull me out. Say it with me! Uh-oh! Spoilers!</p>
+        </div>
+    );
+};
+
+export default About;
